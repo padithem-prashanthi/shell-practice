6e1f84ffa502a8f7736c39090e02642d290d254d
padithem-prashanthi <mail2kethana@gmail.com>
2025-10-03 05:13:06 -0500

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+PERSON1="Alice"
+PERSON2="Bob"
+echo "$PERSON1:: Hi $PERSON2 How are you?"
+echo "$PERSON2:: I am fine $PERSON1 how are you?"
+echo "$PERSON1:: What are you learning recently?"
+echo "$PERSON2:: I am learning shell scripting $PERSON1"
