8b2ec10a4d7c20597442c151529c95ce213f1afe
padithem-prashanthi <mail2kethana@gmail.com>
2025-10-03 04:09:50 -0500

    conversationpeople

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Alice:: Hi Bob How are you?"
+echo "Bob:: I am fine Alice how are you?"
+echo "Alice:: What are you learning recently?"
+echo "Bob:: I am learning shell scripting Alice"
