ed0b8167e13e41868b38b85b4f7700cba682aba3
padithem-prashanthi <mail2kethana@gmail.com>
2025-10-03 05:19:48 -0500

    argumentscode

---
+++ 04-aruguments.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hi $PERSON2 How are you?"
+echo "$PERSON2:: I am fine $PERSON1 how are you?"
+echo "$PERSON1:: What are you learning recently?"
+echo "$PERSON2:: I am learning shell scripting $PERSON1"
