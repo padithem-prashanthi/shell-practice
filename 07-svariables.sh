e5f7172c083cb3bc880aaf517771d5a934723e45
padithem-prashanthi <mail2kethana@gmail.com>
2025-10-04 10:16:16 -0500

    changes

--- 07-svariables.sh
+++ 07-svariables.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-DATE=$(date)
-echo "Current date :" $DATE
+#DATE=$(date)
+#echo "Current date :" $DATE
+START_TIME=$(date +%s)
+echo $START_TIME
+
+sleep 10 
+
+END_TIME=$(date +%s)
+echo $END_TIME
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "Script executed in: $TOTAL_TIME Seconds"
---
+++ 08-special-var.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#default varaiable by system
+
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*"
+
+echo "Name of the script: $0"
+echo "Current working directory: $PWD"
+echo "Who is running this script: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of this script" $$""\
+sleep 50 &
+echo "PID of the last command in background is : $!"
