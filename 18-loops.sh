5acc66789b66a7bf7fa45695739e28b73755bca5
padithem-prashanthi <mail2kethana@gmail.com>
2025-10-04 21:27:21 -0500

    changes

---
+++ 18-loops.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d  "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo -e "$R ERROR:: Please run this script with root privelge $N"
+    exit 1 # filure is other than 0 to exit
+fi
+
+VALIDATE(){ # Functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then  #previous command exit status if 0 success otherwise failed
+        echo -e "Installing  $2 ..... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "Installing $2 .... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+#$@ - to get all input parameters
+
+for package in $@
+do
+    # check package is already installed or not
+    dnf list installed $package &>>$LOG_FILE
+   
+    #if exit status is 0, already installed. -ne 0 need to install
+    if [ $? -ne 0 ]; then
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package"
+    else
+        echo -e "$package already installed...$Y SKIPPING $N"
+
+done
