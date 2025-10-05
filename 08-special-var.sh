ddda9a4dcd25956e7ace59373e13ea8f79aed049
padithem-prashanthi <mail2kethana@gmail.com>
2025-10-04 21:38:03 -0500

    changes

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -8,6 +8,6 @@ echo "Name of the script: $0"
 echo "Current working directory: $PWD"
 echo "Who is running this script: $USER"
 echo "Home directory of user: $HOME"
-echo "PID of this script" $$""\
+echo "PID of this script" $$"
 sleep 50 &
 echo "PID of the last command in background is : $!"
